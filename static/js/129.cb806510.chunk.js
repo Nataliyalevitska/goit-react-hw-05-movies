88a0ad0ed95adb60ed78dadb5125c7fd77d6cc9f
Nataliyalevitska <90378512+Nataliyalevitska@users.noreply.github.com>
2022-08-08 13:57:39 +0000

    Deploying to gh-pages from @ Nataliyalevitska/goit-react-hw-05-movies@2cc33c4153846c0f60b7c71b403c39717136d876 🚀

---
+++ static/js/129.cb806510.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[129],{2129:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var u=n(184);function r(){return(0,u.jsx)("h1",{children:" Page Not Found"})}}}]);
+//# sourceMappingURL=129.cb806510.chunk.js.map
